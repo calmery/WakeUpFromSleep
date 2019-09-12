1dc74a05261fbc34089e657ad7f0e2a368a95e34
Marei Kikukawa <contact@calmery.me>
2019-09-13 02:13:40 +0900

    Execute the callback function when powered on

--- binding.gyp
+++ binding.gyp
@@ -1,6 +1,9 @@
 {
   "targets": [
     {
+      "link_settings": {
+        "libraries": ["-framework IOKit", "-framework CoreFoundation"]
+      },
       "sources": ["src/main.cc"],
       "target_name": "main"
     }
--- src/main.cc
+++ src/main.cc
@@ -1,26 +1,73 @@
 #include <node.h>
+#include <IOKit/IOMessage.h>
+#include <IOKit/pwr_mgt/IOPMLib.h>
 
 using v8::Context;
+using v8::Exception;
 using v8::Function;
 using v8::FunctionCallbackInfo;
 using v8::Isolate;
 using v8::Local;
 using v8::NewStringType;
-using v8::Null;
 using v8::Object;
 using v8::String;
 using v8::Value;
 
+Isolate *isolate;
+Local<Function> callbackFunction;
+IONotificationPortRef thePortRef;
+io_connect_t root_port;
+io_object_t notifier;
+
+// Reference:
+// https://developer.apple.com/documentation/iokit/ioserviceinterestcallback
+void callback(void *refcon, io_service_t service, uint32_t messageType,
+              void *messageArgument) {
+
+  if (messageType == kIOMessageSystemHasPoweredOn) {
+    CFRunLoopRemoveSource(CFRunLoopGetCurrent(),
+                          IONotificationPortGetRunLoopSource(thePortRef),
+                          kCFRunLoopCommonModes);
+    IODeregisterForSystemPower(&notifier);
+    IOServiceClose(root_port);
+    IONotificationPortDestroy(thePortRef);
+    CFRunLoopStop(CFRunLoopGetMain());
+
+    callbackFunction->Call(isolate->GetCurrentContext(), Null(isolate), 0, NULL)
+        .ToLocalChecked();
+  }
+}
+
 void method(const FunctionCallbackInfo<Value> &arguments) {
-  Isolate *isolate = arguments.GetIsolate();
-  Local<Context> context = isolate->GetCurrentContext();
-  Local<Function> callback = Local<Function>::Cast(arguments[0]);
-  const unsigned argumentCount = 1;
-  Local<Value> argumentVector[argumentCount] = {
-      String::NewFromUtf8(isolate, "Hello World", NewStringType::kNormal)
-          .ToLocalChecked()};
-  callback->Call(context, Null(isolate), argumentCount, argumentVector)
-      .ToLocalChecked();
+  isolate = arguments.GetIsolate();
+
+  if (arguments.Length() == 0) {
+    isolate->ThrowException(Exception::Error(
+        String::NewFromUtf8(isolate, "Wrong number of arguments",
+                            NewStringType::kNormal)
+            .ToLocalChecked()));
+    return;
+  }
+
+  callbackFunction = Local<Function>::Cast(arguments[0]);
+
+  // Reference:
+  // https://developer.apple.com/documentation/iokit/1557114-ioregisterforsystempower
+  root_port = IORegisterForSystemPower(NULL, &thePortRef, callback, &notifier);
+
+  if (!root_port) {
+    isolate->ThrowException(Exception::Error(
+        String::NewFromUtf8(isolate,
+                            "IORegisterForSystemPower execution Failed",
+                            NewStringType::kNormal)
+            .ToLocalChecked()));
+    return;
+  }
+
+  CFRunLoopAddSource(CFRunLoopGetCurrent(),
+                     IONotificationPortGetRunLoopSource(thePortRef),
+                     kCFRunLoopCommonModes);
+  CFRunLoopRun();
 }
 
 void initialize(Local<Object> exports, Local<Object> module) {
