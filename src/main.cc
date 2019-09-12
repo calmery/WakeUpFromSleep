1ddc0f7e1b186901af05cd89d4af50750b44b70f
Marei Kikukawa <contact@calmery.me>
2019-09-13 01:50:02 +0900

    Change the addon to execute the callback function

--- src/main.cc
+++ src/main.cc
@@ -1,18 +1,26 @@
 #include <node.h>
 
+using v8::Context;
+using v8::Function;
 using v8::FunctionCallbackInfo;
 using v8::Isolate;
 using v8::Local;
 using v8::NewStringType;
+using v8::Null;
 using v8::Object;
 using v8::String;
 using v8::Value;
 
-void method(const FunctionCallbackInfo<Value> &args) {
-  Isolate *isolate = args.GetIsolate();
-  args.GetReturnValue().Set(
+void method(const FunctionCallbackInfo<Value> &arguments) {
+  Isolate *isolate = arguments.GetIsolate();
+  Local<Context> context = isolate->GetCurrentContext();
+  Local<Function> callback = Local<Function>::Cast(arguments[0]);
+  const unsigned argumentCount = 1;
+  Local<Value> argumentVector[argumentCount] = {
       String::NewFromUtf8(isolate, "Hello World", NewStringType::kNormal)
-          .ToLocalChecked());
+          .ToLocalChecked()};
+  callback->Call(context, Null(isolate), argumentCount, argumentVector)
+      .ToLocalChecked();
 }
 
 void initialize(Local<Object> exports, Local<Object> module) {
